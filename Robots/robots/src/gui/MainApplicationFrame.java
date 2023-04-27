7000a2339de470014f762d14c7d7b3508a6391cd
busyboyent <busyboyent@gmail.com>
2023-04-27 21:53:44 +0500

    меню фрэйм стэйт

--- Robots/robots/src/gui/MainApplicationFrame.java
+++ Robots/robots/src/gui/MainApplicationFrame.java
@@ -2,58 +2,132 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.*;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-import jdk.jshell.spi.ExecutionControl;
 import log.Logger;
-import primitives.IRobot;
 
 /**
  * Что требуется сделать:
  * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
  */
-public class MainApplicationFrame extends JFrame
+public class MainApplicationFrame extends JFrame implements SerializableFrame
 {
     private final JDesktopPane desktopPane = new JDesktopPane();
-    private GameWindow gameWindow;
-    public MainApplicationFrame() {
+    private static final String fileName = "frame.state";
+
+    public MainApplicationFrame()
+    {
         //Make the big window be indented 50 pixels from each edge
         //of the screen.
         int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-                screenSize.width  - inset*2,
-                screenSize.height - inset*2);
+                screenSize.width - inset * 2,
+                screenSize.height - inset * 2);
 
         setContentPane(desktopPane);
-
 
 
         LogWindow logWindow = createLogWindow();
         addWindow(logWindow);
 
-        gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.setSize(400, 400);
         addWindow(gameWindow);
 
         setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        this.addWindowListener(new WindowAdapter() {public void windowClosing(WindowEvent we) {
-            close(); }});
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                super.windowClosing(e);
+                onExit();
+            }
+
+            @Override
+            public void windowOpened(WindowEvent e)
+            {
+                super.windowOpened(e);
+                onOpen();
+            }
+        });
+    }
+
+    private void onOpen()
+    {
+        String directory = System.getProperty("user.home");
+        File file = new File(directory, fileName);
+        if (!file.exists())
+            return;
+        FrameState state=null;
+        try (FileInputStream fileStream = new FileInputStream(file);ObjectInputStream stream=new ObjectInputStream(fileStream))
+        {
+            state= (FrameState) stream.readObject();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        if (state !=null)
+            setState(state);
+    }
+
+    private void setState(FrameState state)
+    {
+        for (InternalFrameState internalState :
+                state.getFrames())
+        {
+            // if (internalState.name.Equals())
+        }
+    }
+
+    private void onExit()
+    {
+        saveState();
+    }
+
+    private void saveState()
+    {
+        String directory = System.getProperty("user.home");
+        File file = new File(directory, fileName);
+        if (!file.exists())
+            try
+            {
+                file.createNewFile();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        try (FileOutputStream fileStream = new FileOutputStream(file); ObjectOutputStream stream = new ObjectOutputStream(fileStream))
+        {
+            serialize(stream);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     protected LogWindow createLogWindow()
     {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
+        logWindow.setLocation(10, 10);
         logWindow.setSize(300, 800);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
@@ -67,85 +141,89 @@ public class MainApplicationFrame extends JFrame
         frame.setVisible(true);
     }
 
+//    protected JMenuBar createMenuBar() {
+//        JMenuBar menuBar = new JMenuBar();
+//
+//        //Set up the lone menu.
+//        JMenu menu = new JMenu("Document");
+//        menu.setMnemonic(KeyEvent.VK_D);
+//        menuBar.add(menu);
+//
+//        //Set up the first menu item.
+//        JMenuItem menuItem = new JMenuItem("New");
+//        menuItem.setMnemonic(KeyEvent.VK_N);
+//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+//                KeyEvent.VK_N, ActionEvent.ALT_MASK));
+//        menuItem.setActionCommand("new");
+////        menuItem.addActionListener(this);
+//        menu.add(menuItem);
+//
+//        //Set up the second menu item.
+//        menuItem = new JMenuItem("Quit");
+//        menuItem.setMnemonic(KeyEvent.VK_Q);
+//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+//                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
+//        menuItem.setActionCommand("quit");
+////        menuItem.addActionListener(this);
+//        menu.add(menuItem);
+//
+//        return menuBar;
+//    }
+
     private JMenuBar generateMenuBar()
     {
         JMenuBar menuBar = new JMenuBar();
 
-        var lookAndFeelMenu = createJMenu("Режим отображения",
-                KeyEvent.VK_V, "Управление режимом отображения приложения");
-        var testMenu = createJMenu("Тесты", KeyEvent.VK_T, "Тестовые команды");
-        var programMenu = createJMenu("Программа", KeyEvent.VK_B, "Помощь");
+        JMenu fileMenu = createMenu("Файл", KeyEvent.VK_T, "Файл");
+        JMenuItem close = createMenuItem("Выход", KeyEvent.VK_S, (event) ->
         {
-            var systemLookAndFeel = createJMenuItem("Системная схема", KeyEvent.VK_S,
-                    (event) -> {
-                        setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                        this.invalidate(); });
-            lookAndFeelMenu.add(systemLookAndFeel);
+            Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(
+                    new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+        });
+        fileMenu.add(close);
+        menuBar.add(fileMenu);
 
-            var crossplatformLookAndFeel = createJMenuItem("Универсальная схема", KeyEvent.VK_S,
-                    (event) -> {
-                        setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-                        this.invalidate();});
-            lookAndFeelMenu.add(crossplatformLookAndFeel);
-
-            var addLogMessageItem = createJMenuItem("Сообщение в лог", KeyEvent.VK_S,
-                    (event) -> {Logger.debug("Новая строка");});
-            testMenu.add(addLogMessageItem);
-
-            var exit = createJMenuItem("Выход", KeyEvent.VK_E, (event) ->{ close();
-            });
-            programMenu.add(exit);
-
-            var addRobotLogic = createJMenuItem("Загрузить логику робота", KeyEvent.VK_L,
-                    (event) -> {chooseFile();});
-            programMenu.add(addRobotLogic);
-        }
-
-        menuBar.add(programMenu);
+        JMenu lookAndFeelMenu = createMenu("Режим отображения", KeyEvent.VK_V, "Управление режимом отображения приложения");
+        JMenuItem systemLookAndFeel = createMenuItem("Системная схема", KeyEvent.VK_S, (event) ->
+        {
+            setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            this.invalidate();
+        });
+        JMenuItem crossplatformLookAndFeel = createMenuItem("Универсальная схема", KeyEvent.VK_S, (event) ->
+        {
+            setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            this.invalidate();
+        });
+        lookAndFeelMenu.add(systemLookAndFeel);
+        lookAndFeelMenu.add(crossplatformLookAndFeel);
         menuBar.add(lookAndFeelMenu);
+
+        JMenu testMenu = createMenu("Тесты", KeyEvent.VK_T, "Тестовые команды");
+        JMenuItem addLogMessageItem = createMenuItem("Сообщение в лог", KeyEvent.VK_S, (event) ->
+                Logger.debug("Новая строка")
+        );
+        testMenu.add(addLogMessageItem);
         menuBar.add(testMenu);
 
         return menuBar;
     }
 
-    private void chooseFile(){
-        var chooser = new JFileChooser();
-        var filter = new FileNameExtensionFilter("Description", "class");
-        chooser.setFileFilter(filter);
-        var result = chooser.showDialog(null, "Загрузить логику бота");
-        if (result == JFileChooser.APPROVE_OPTION){
-            var file = chooser.getSelectedFile();
-            var path = file.getPath();
-
-        }
+    private JMenu createMenu(String name, int mnemonic, String description)
+    {
+        JMenu lookAndFeelMenu = new JMenu(name);
+        lookAndFeelMenu.setMnemonic(mnemonic);
+        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
+                description);
+        return lookAndFeelMenu;
     }
 
-    private void close(){
-        UIManager.put("OptionPane.yesButtonText"   , "Да"    );
-        UIManager.put("OptionPane.noButtonText"    , "Нет"   );
-        var result = JOptionPane.showConfirmDialog(this,
-                "Вы Уверены?",
-                "ОКНО ВЫХОДА",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.WARNING_MESSAGE);
-        if(result == JOptionPane.YES_OPTION){
-            System.exit(0);
-        }
+    private JMenuItem createMenuItem(String text, int mnemonic, ActionListener listener)
+    {
+        JMenuItem resultItem = new JMenuItem(text, mnemonic);
+        resultItem.addActionListener(listener);
+        return resultItem;
     }
 
-    private JMenuItem createJMenuItem(String text, int mnemonic, ActionListener action){
-        var jMenuItem = new JMenuItem(text, mnemonic);
-        jMenuItem.addActionListener(action);
-        return jMenuItem;
-    }
-
-    private JMenu createJMenu(String menuName, int mnemonic, String accessibleDescription){
-        var jmenu = new JMenu(menuName);
-        jmenu.setMnemonic(mnemonic);
-        jmenu.getAccessibleContext().setAccessibleDescription(
-                accessibleDescription);
-        return jmenu;
-    }
 
     private void setLookAndFeel(String className)
     {
@@ -153,11 +231,28 @@ public class MainApplicationFrame extends JFrame
         {
             UIManager.setLookAndFeel(className);
             SwingUtilities.updateComponentTreeUI(this);
-        }
-        catch (ClassNotFoundException | InstantiationException
-               | IllegalAccessException | UnsupportedLookAndFeelException e)
+        } catch (ClassNotFoundException | InstantiationException
+                 | IllegalAccessException | UnsupportedLookAndFeelException e)
         {
             // just ignore
         }
+    }
+
+    @Override
+    public void serialize(ObjectOutputStream stream) throws IOException
+    {
+        FrameState state = new FrameState(this);
+        stream.writeObject(state);
+    }
+
+    @Override
+    public void restore(ObjectInputStream stream)
+    {
+
+    }
+
+    public JInternalFrame[] getAllFrames()
+    {
+        return desktopPane.getAllFrames();
     }
 }
